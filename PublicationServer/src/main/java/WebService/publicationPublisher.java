5e49ccc84b28446ccdf061d6891b611a7ce48824
Ivan Cheltsov <chip.pragma@gmail.com>
2019-12-17 02:24:21 +0300

    Парсер коммандной строки

--- PublicationServer/src/main/java/WebService/publicationPublisher.java
+++ PublicationServer/src/main/java/WebService/publicationPublisher.java
@@ -1,19 +1,52 @@
 package WebService;
 
+import org.apache.commons.cli.*;
+
 import javax.xml.ws.Endpoint;
 
 public class publicationPublisher {
+    public static final String OPTION_WSDL_URL = "wsdl_url";
+    public static final String OPTION_DB_URL = "db_url";
+
+    public static CommandLine parseArgs(String[] args) {
+        Options opts = new Options();
+
+        Option wsdlUrl = new Option(OPTION_WSDL_URL, true, "URL-address of WSDL-service");
+        wsdlUrl.setRequired(true);
+        opts.addOption(wsdlUrl);
+
+        Option dbUrl = new Option(OPTION_DB_URL, true, "URL-address for Data Base");
+        dbUrl.setRequired(true);
+        opts.addOption(dbUrl);
+
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLineParser parser = new DefaultParser();
+
+        try {
+            return parser.parse(opts, args);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            formatter.printHelp("publication_publisher", opts);
+            System.exit(1);
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
-        // запускаем веб-сервер на порту 1986
-        // и по адресу, указанному в первом аргументе,
-        // запускаем веб-сервис, передаваемый во втором аргументе
-       try{
-           Endpoint.publish("http://0.0.0.0:1986/PublicationService", new publicationImpl());
-           System.out.println("Server run!");
-       }catch (Exception ex){
-           System.out.println(ex.getMessage());
-       }
+        // парсер параметров
+        var cmd = parseArgs(args);
+        System.out.println(String.format("WSDL: %s", cmd.getOptionValue(OPTION_WSDL_URL)));
+        System.out.println(String.format("DataBase: %s", cmd.getOptionValue(OPTION_DB_URL)));
+
+        try {
+            // запускаем веб-сервис, передаваемый во втором аргументе
+            Endpoint.publish(cmd.getOptionValue(OPTION_WSDL_URL), new publicationImpl());
+            System.out.println("Server run!");
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 
     }
 }
-//http://192.168.0.126:1986/PublicationService?wsdl
